93a534abcd804fa8a776e58f7d64acfff8c8f4a4
Magnaar <io.magnaar@gmail.com>
2017-10-22 20:23:37 +0200

    volume to 1.0

--- src/Data.js
+++ src/Data.js
@@ -58,8 +58,8 @@ export default class Data
 	static _loadSettings()
 	{
 		Data.settings = new Settings({
-			effectVolume: 0.5,
-			musicVolume: 0.5
+			effectVolume: 1.0,
+			musicVolume: 1.0
 		})
 	}
 
